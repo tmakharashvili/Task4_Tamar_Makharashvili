fb2609ec191ef05a817f12262bc1d983df668c90
tamar makharashvili <tamarmakharashvili@credo.ge>
2024-10-17 01:36:21 +0400

    Task2

---
+++ src/test/java/Task2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char inputChar = scanner.next().charAt(0);
+
+        switch (inputChar) {
+            case 'a':
+                System.out.println("xmovani");
+                break;
+            case 'e':
+                System.out.println("xmovani");
+                break;
+            case 'i':
+                System.out.println("xmovani");
+                break;
+            case 'o':
+                System.out.println("xmovani");
+                break;
+            case 'u':
+                System.out.println("xmovani");
+                break;
+
+            default:
+                System.out.println("tanxmovani");
+        }
+    }
+}
